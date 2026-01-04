f97e2c13c9c513417b0b7fd20d2b2850f42fb78f
Alice Emily <alice.nascimento@bsatech.io>
2026-01-04 15:22:38 -0300

    feat: create database with redis

--- services/database.py
+++ services/database.py
@@ -1,3 +1,24 @@
 '''
 Docstring for services.database
 '''
+import os
+import redis.asyncio as redis
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+redis_client = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=REDIS_DB,
+    decode_response=True
+)
+
+async def test_connect():
+    try:
+        pong = await redis_client.ping()
+        print("Conectado: ", pong)
+    except Exception as e:
+        print("Falha ao conectar com Redis: ", e)
+        raise
