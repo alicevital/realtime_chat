1312e72632f5db3e3e74ab8bb98687018c4cefb1
alice.nascimento@bsatech.io <alice.nascimento@bsatech.io>
2026-01-08 17:14:17 -0300

    refactor: exclui classe nao utilizada

--- services/schemas/user_schema.py
+++ services/schemas/user_schema.py
@@ -1,22 +1,13 @@
 '''
 Docstring for services.schemas(Schemas para futura integração com banco MongoDB)
 '''
-from typing import Optional
 from pydantic import BaseModel
-from datetime import datetime
 
 class UserRequest(BaseModel):
     username: str
     name: str
     password: str
     is_admin: bool
-
-class UserResponse(BaseModel):
-    name: str
-    username: str
-    is_admin: bool
-    id: Optional[str] = None
-    date_created: datetime
 
 class UserLogin(BaseModel):
     name: str
