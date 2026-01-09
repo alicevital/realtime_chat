f32d20882e77566016fc88a6ecb568f9daf3acad
alice.nascimento@bsatech.io <alice.nascimento@bsatech.io>
2026-01-09 13:55:03 -0300

    docs: add docstrings em database file

--- services/utils/database.py
+++ services/utils/database.py
@@ -1,13 +1,17 @@
-'''
-Docstring for services.database
-'''
 import os
 import redis.asyncio as redis
 
+
+'''
+    Host, Porta e nome do database redis
+'''
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
 
+'''
+    redis_client recebe a classe do Redis e as variáveis de ambiente anteriores
+'''
 redis_client = redis.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
@@ -16,6 +20,13 @@ redis_client = redis.Redis(
 )
 
 async def test_connect():
+    '''
+        Teste de conexão do redis;
+        Entre no terminal com redis-cli;
+        Escreva ping;
+        Se estiver conectado, retornará "PONG"
+        Se não, retornará a mensagem de erro.
+    '''
     try:
         pong = await redis_client.ping()
         print("Conectado: ", pong)
