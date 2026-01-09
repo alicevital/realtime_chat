9adbbe36c86327580f7f74e14efafc7e26b956bb
Alice Emily <alice.nascimento@bsatech.io>
2026-01-08 23:20:17 -0300

    feat: add rooms create

--- services/main.py
+++ services/main.py
@@ -4,6 +4,7 @@ from services.routes.room_route import router as room_router
 from services.routes.user_route import router as user_router
 from services.websocket_manager import manager
 import logging
+from services.database import redis_client
 
 
 app = FastAPI(tittle="Realtime Chat")
@@ -13,53 +14,44 @@ app.include_router(room_router)
 
 @app.on_event("startup")
 async def startup():
+    await redis_client.sadd("rooms", "global")
     asyncio.create_task(manager.start_redis())
 
-# endpoint criar user recebe "username, password, is_admin=True or False"
-# endpoint de login (username e password) e atribue logged = True
-
-
-# criar um endpoint que create-room no banco do redis
-# só pode criar a sala se o user for admin = True e logged = True
 
 @app.websocket("/ws/global")
-async def chat_global(websocket: WebSocket, UserLogin):
-    # aguardar o username para adicioanr uma melhor UX
-    # verificar se username existe no redis
-    # remover a criação do channel por força bruta
+async def chat_global(websocket: WebSocket, username: str):
+
+    if not await redis_client.sismember("rooms", "global"):
+        await websocket.close(code=1011)
+        return
+    
     channel = "chat:global"
+
     client_id = await manager.connect(websocket, channel)
+
     try:
         while True:
             data = await websocket.receive_text()
-            logging.warning(f"Mensagem recebida do client {client_id}: {data}")
-            await manager.publish(f"Client {client_id}: {data}", channel)
-
-            
+            await manager.publish(f"{username}: {data}", channel)
     except WebSocketDisconnect:
         manager.disconnect(client_id, channel)
-        await manager.publish(f"Client {client_id} saiu do chat.", channel)
-
-    except Exception as e:
-        logging.error(e)
 
 
 @app.websocket("/ws/group/{room_name}")
-# async def chat_private(websocket: WebSocket, PrivateChatDTO):
-async def chat_room(websocket: WebSocket, channel, room_name):
-    channel = f"chat:group:{room_name}"
+
+async def chat_room(websocket: WebSocket, room_name: str, username: str):
+
+    if not await redis_client.sismember("rooms", room_name):
+        await websocket.close(code=1008)
+        return
+    
+    channel = f"chat: group:{room_name}"
     client_id = await manager.connect(websocket, channel)
-    # adicionar o username e room_name no DTO
-    # validar o room_name recebido do DTO, se existir no redis
-    # verificar se username existe no redis
 
     try:
         while True:
             data = await websocket.receive_text()
-
-            await manager.publish(f"Client {client_id}: {data}", channel)
-
-
+            await manager.publish(f"{username}: {data}", channel)
     except WebSocketDisconnect:
         manager.disconnect(client_id, channel)
 
--- services/routes/room_route.py
+++ services/routes/room_route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services.schemas.room_schema import RoomRequest, RoomResponse
 from services.database import redis_client
 
@@ -6,11 +6,28 @@ from services.database import redis_client
 router = APIRouter()
 
 @router.post("/room/create")
-def create_room(room: RoomRequest):
-    # is_admin = user_data["is_admin"].lower() == "true"
+async def create_room(room_name: str, username: str):
 
-    return room
+    user_key = f"user:{username}"
+
+    if not await redis_client.exists(user_key):
+        raise HTTPException(404, "Usuário não existe")
+    
+    user_data = await redis_client.hgetall(user_key)
+
+    if user_data["is_admin"] != "true":
+        raise HTTPException(403, "Apenas admins podem criar salas")
+    
+    if not await redis_client.sismember("logged_users", username):
+        raise HTTPException(403, "Usuário não está logado")
+    
+    if await redis_client.sismember("rooms", room_name):
+        raise HTTPException(400, "Sala já existe")
+    
+    await redis_client.sadd("rooms", room_name)
+
+    return {"message": f"Sala '{room_name}' criada!"}
 
 @router.get("/get/rooms")
 def get_room(room: RoomResponse):
-    return room
+    pass
